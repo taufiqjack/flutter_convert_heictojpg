83b0f161eeb85b90ad1c0c75e263171bb3edb36e
taufiqjack <taufiqdwic@gmail.com>
2025-10-17 14:46:22 +0700

    update packages

--- lib/common/views/home_view.dart
+++ lib/common/views/home_view.dart
@@ -1,10 +1,11 @@
 import 'dart:io';
-
-import 'package:flutter/foundation.dart';
+import 'package:any_image_view/any_image_view.dart';
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
-import 'package:heic_to_jpg/heic_to_jpg.dart';
-import 'package:image_picker/image_picker.dart';
-import 'package:path_provider/path_provider.dart';
+import 'package:flutter_convert_heictojpg/core/component/toast.dart';
+import 'package:heif_converter/heif_converter.dart';
+import 'package:image_gallery_saver_plus/image_gallery_saver_plus.dart';
+import 'package:path/path.dart' as path;
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
@@ -14,95 +15,158 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
-  File? imageFile;
   dynamic pickImageError;
-  final picker = ImagePicker();
+
   String? convertPath;
   String? jpgPath;
+  final httpClient = HttpClient();
 
-  void _onSelectImagePicker(ImageSource source) async {
+  File? heicFiles;
+  FilePickerResult? result;
+  List<String> convertedPaths = [];
+
+  Future pickImageConvert() async {
+    result = await FilePicker.platform.pickFiles(
+      type: FileType.custom,
+      allowMultiple: true,
+      allowedExtensions: ['heic', 'HEIC'],
+    );
+    debugPrint('list path ${result?.files.first.path}');
+
+    if (result == null || result?.files == null) {
+      debugPrint('User canceled the picker.');
+      return null;
+    }
+    setState(() {});
+  }
+
+  Future<List<String>?> saveImageConvert() async {
     try {
-      final pickedFile = await picker.pickImage(source: source);
-      if (pickedFile != null) {
-        imageFile = File(pickedFile.path);
-      } else {
-        if (kDebugMode) {
-          print('No image selected');
+      for (var file in result!.files) {
+        if (file.path == null) continue;
+
+        File heicFile = File(file.path!);
+
+        final convertedPath = await HeifConverter.convert(
+          heicFile.path,
+          format: 'jpg',
+        );
+
+        if (convertedPath != null) {
+          convertedPaths.add(convertedPath);
+
+          final bytes = await File(convertedPath).readAsBytes();
+          await ImageGallerySaverPlus.saveImage(
+            bytes,
+            quality: 100,
+            name: path.basenameWithoutExtension(heicFile.path),
+          );
+
+          debugPrint('✅ Saved: $convertedPath');
+          if (mounted) {
+            toast(context, '✅ Saved: $convertedPath');
+          }
         }
       }
+      return convertedPaths;
     } catch (e) {
-      pickImageError = e;
+      debugPrint('❌ Error: $e');
+      if (mounted) {
+        toast(context, '$e');
+      }
+      return [];
     }
-    if (imageFile != null) {}
-    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          imageFile == null
-              ? const SizedBox()
-              : ClipRRect(
-                  borderRadius: BorderRadius.circular(10),
-                  child: Image.file(
-                    File(imageFile!.path),
-                    height: MediaQuery.of(context).size.height / 2,
-                  ),
-                ),
-          const SizedBox(
-            height: 20,
+        appBar: AppBar(
+          title: Text(
+            'CONVERT HEIC TO JPG',
+            style: TextStyle(fontWeight: FontWeight.w700),
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
+          centerTitle: true,
+        ),
+        body: Padding(
+          padding: EdgeInsetsGeometry.only(top: 30),
+          child: Center(
+              child: Column(
             children: [
-              InkWell(
-                onTap: () {
-                  _onSelectImagePicker(ImageSource.gallery);
-                },
-                child: Container(
-                  height: 40,
-                  width: MediaQuery.of(context).size.width / 3,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(8),
-                    color: Color.fromARGB(255, 0, 185, 172),
-                  ),
-                  child: const Center(
-                    child: Text(
-                      'Pilih Foto',
-                      style: TextStyle(color: Colors.white),
+              result == null
+                  ? const SizedBox()
+                  : GridView.builder(
+                      padding: EdgeInsets.only(left: 10),
+                      shrinkWrap: true,
+                      itemCount: result?.files.length,
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 3,
+                          childAspectRatio: 3 / 3,
+                          mainAxisSpacing: 10,
+                          crossAxisSpacing: 10),
+                      itemBuilder: (context, index) => ClipRRect(
+                        borderRadius: BorderRadius.circular(10),
+                        child: AnyImageView(
+                          height: 230,
+                          width: 200,
+                          borderRadius: BorderRadius.circular(10),
+                          fadeDuration: Duration(milliseconds: 300),
+                          imagePath: result!.files[index].path!,
+                          fit: BoxFit.fill,
+                        ),
+                      ),
                     ),
-                  ),
-                ),
-              ),
-              InkWell(
-                onTap: () async {
-                  convertPath = (await getTemporaryDirectory()).path;
-                  jpgPath = await HeicToJpg.convert(imageFile!.path);
-                  imageFile = File(jpgPath!);
-                },
-                child: Container(
-                  height: 40,
-                  width: MediaQuery.of(context).size.width / 3,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(8),
-                    color: Color.fromARGB(255, 0, 185, 172),
-                  ),
-                  child: const Center(
-                    child: Text(
-                      'Convert ke Jpg',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ),
-                ),
+              const SizedBox(
+                height: 20,
               ),
             ],
-          )
-        ],
-      )),
-    );
+          )),
+        ),
+        floatingActionButton: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Padding(
+              padding: EdgeInsetsGeometry.only(left: 30),
+              child: InkWell(
+                onTap: () {
+                  pickImageConvert();
+                },
+                child: Container(
+                  height: 40,
+                  width: MediaQuery.of(context).size.width / 3,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    color: Color.fromARGB(255, 0, 185, 172),
+                  ),
+                  child: const Center(
+                    child: Text(
+                      'Select Photos',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            InkWell(
+              onTap: () async {
+                saveImageConvert();
+              },
+              child: Container(
+                height: 40,
+                width: MediaQuery.of(context).size.width / 3,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8),
+                  color: Color.fromARGB(255, 0, 185, 172),
+                ),
+                child: const Center(
+                  child: Text(
+                    'Convert to JPG',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ));
   }
 }
---
+++ lib/core/component/toast.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+void toast(ctx, String text) async {
+  FToast fToast = FToast();
+  fToast.init(ctx);
+  fToast.showToast(
+      child: Container(
+    padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 12.0),
+    decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15),
+        boxShadow: [
+          BoxShadow(color: Color(0xFFDDDDDD).withValues(alpha: 0.8))
+        ]),
+    child: Text(
+      text,
+      style: TextStyle(color: Colors.black),
+    ),
+  ));
+}
